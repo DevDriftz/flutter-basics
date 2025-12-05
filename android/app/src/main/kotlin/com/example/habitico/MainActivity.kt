2009800d08119e431203cc69778c4698a25206fb
DevDriftz <akshatsharmabhrigu@gmail.com>
2025-12-05 20:38:53 +0530

    first commit Basic Structure

---
+++ android/app/src/main/kotlin/com/example/habitico/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.habitico
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text('Hello World',style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),),
+        ),
+      ),
+    );
+  }
+}
